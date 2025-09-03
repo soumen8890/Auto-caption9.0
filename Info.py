ae892de0b8bd1cb3b495ac7379d39e6061a4855d
soumen8890 <130748304+soumen8890@users.noreply.github.com>
2025-09-03 23:23:20 +0530

    Update Info.py

--- Info.py
+++ Info.py
@@ -1,10 +1,10 @@
 import os
 
 class Info:
-    API_ID = os.getenv("API_ID", "your_api_id")
-    API_HASH = os.getenv("API_HASH", "your_api_hash")
-    BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token")
-    OWNER_ID = int(os.getenv("OWNER_ID", "123456789"))
+    API_ID = os.getenv("API_ID", "20919625")
+    API_HASH = os.getenv("API_HASH", "40168846bf06f4ff443f0f7a4182bf8d")
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+    OWNER_ID = int(os.getenv("OWNER_ID", "6233910543"))
 
-    MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://user:pass@cluster/db")
+    MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://hicit73592:Sf3ketW4B0pFTgvd@cluster0.aa2c7wh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DATABASE_NAME = os.getenv("DATABASE_NAME", "captionbot")
