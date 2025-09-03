88d0ef82aa5e7fdea6d1b5b3d95191d185802f2c
soumen8890 <130748304+soumen8890@users.noreply.github.com>
2025-09-04 00:15:13 +0530

    Update buttons.py

--- handlers/buttons.py
+++ handlers/buttons.py
@@ -1,6 +1,11 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from Info import Info
+from pyrogram import Client, filters
+
+@Client.on_message(filters.command("start"))
+async def start_handler(client, message):
+    await message.reply_text("👋 Hello! I am alive.")
 
 @Client.on_message(filters.command("setbutton") & filters.user(Info.OWNER_ID))
 async def set_button(client, message):
