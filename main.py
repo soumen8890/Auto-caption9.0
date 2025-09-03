f8d8130ab740a20cbf54ed6d7abdede75999bc97
soumen8890 <130748304+soumen8890@users.noreply.github.com>
2025-09-03 23:41:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
+from multiprocessing import Process
 from pyrogram import Client
 from Info import Info
 import handlers.caption
 import handlers.buttons
+import server
 
-app = Client(
-    "AutoCaptionBot",
-    api_id=Info.API_ID,
-    api_hash=Info.API_HASH,
-    bot_token=Info.BOT_TOKEN
-)
+def run_bot():
+    app = Client(
+        "AutoCaptionBot",
+        api_id=Info.API_ID,
+        api_hash=Info.API_HASH,
+        bot_token=Info.BOT_TOKEN
+    )
+    app.run()
 
-app.run()
+def run_server():
+    server.app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+
+if __name__ == "__main__":
+    p1 = Process(target=run_bot)
+    p2 = Process(target=run_server)
+
+    p1.start()
+    p2.start()
+
+    p1.join()
+    p2.join()
